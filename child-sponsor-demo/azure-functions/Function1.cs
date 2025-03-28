b341c59507774000c3e1f6538b0c027f2045c2ba
Jason Divis <jdivis@children.org>
2025-03-28 10:47:48 -0500

    Adding a call to get users from a table

--- child-sponsor-demo/azure-functions/Function1.cs
+++ child-sponsor-demo/azure-functions/Function1.cs
@@ -10,4 +10,32 @@ public static class Function1
     {
         log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
     }
+    
+    public static void GetUser(string userInput, ILogger log)
+    {
+        string connectionString = "Server=tcp:wedkjnwerfihnj.database.windows.net,1433;Initial Catalog=wedkjnwerfihnj;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=\"Active Directory Default\";";
+        string query = $"SELECT * FROM Users WHERE Username = '{userInput}'";
+
+        try
+        {
+            using (var connection = new System.Data.SqlClient.SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (var command = new System.Data.SqlClient.SqlCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            log.LogInformation($"User: {reader["Username"]}");
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            log.LogError($"An error occurred: {ex.Message}");
+        }
+    }
 }
